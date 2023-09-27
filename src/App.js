c08e60524695f16ec50890c17628321238291114
gpoveda <geovanny.poveda.cardona@alumnos.upm.es>
2023-09-28 00:37:32 +0200

    first upload for fv
    first upload for fv

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import Home from "./pages/Home" 
+import Details from "./pages/details"
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+function App() {
+
+  return (
+    <Router>
+      <div className="App">
+      <Switch>
+        <Route path ='/' exact component={Home}/>
+        <Route path = '/details/:id' component={Details} render={(props) => <Home {...props} />} >
+          
+        </Route>
+      </Switch>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
